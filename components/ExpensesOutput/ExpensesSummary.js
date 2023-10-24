78ed6058869b3378fd2501fee2cbd3f1f8373a00
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-10-24 18:19:53 +0300

    adding

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,7 +1,17 @@
+import { View, Text } from "react-native";
 
 
-function ExpensesSummary(){
-    return
+
+
+function ExpensesSummary({expenses, periodName}){
+
+    const expensesSum = expenses.reduce((sum, expense) => {
+        return sum + expense.amount;
+    }, 0);
+    return <View>
+    <Text>{periodName}</Text>
+    <Text>{expensesSum.toFixed(2)}€</Text>
+</View>
 }
 
 export default ExpensesSummary;
