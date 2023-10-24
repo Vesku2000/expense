179f660b7b4b61a56253a3998d6b004190b600da
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-10-24 17:31:14 +0300

    works

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import ManageExpenses from './screens/ManageExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview(){
+  return <BottomTabs.Navigator>
+    <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses}/>
+  </BottomTabs.Navigator>
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ManageExpense" component={ManageExpenses}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
