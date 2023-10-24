dd1e3e15922e2eb6622c0d0d2e45ff6ea863341e
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-10-24 21:08:55 +0300

    styling

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,6 +1,7 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
 
 const DUMMY_EXPENSES = [
     {
@@ -42,10 +43,18 @@ const DUMMY_EXPENSES = [
 ]
 
 function ExpensesOutput({expenses, expensesPeriod}) {
-    return  <View>
+    return  <View style={styles.container}>
         <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod}/>
         <ExpensesList expenses={DUMMY_EXPENSES}/>
     </View>
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700,
+
+    }
+})
