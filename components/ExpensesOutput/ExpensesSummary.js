28b77fd4dba2279efb7a1cf541b2e61a8fa49de3
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-10-24 20:51:52 +0300

    displaying items

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -6,7 +6,7 @@ import { View, Text } from "react-native";
 function ExpensesSummary({expenses, periodName}){
 
     const expensesSum = expenses.reduce((sum, expense) => {
-        return sum + expense.amount;
+        return sum + expense.amount
     }, 0);
     return <View>
     <Text>{periodName}</Text>
